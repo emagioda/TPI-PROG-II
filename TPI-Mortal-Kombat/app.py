e0952907a44b2fe94a1d4ce0e921cd65ec872283
JuanB1986 <mj.b86@hotmail.com>
2024-06-23 17:06:47 -0300

    Subo Clase Partida y app.py

---
+++ TPI-Mortal-Kombat/app.py
@@ -0,0 +1,79 @@
+from datos import *
+
+# Muestra el menú principal del juego.
+def menu() -> str:
+    return """
+-- MORTAL KOMBAT --
+1- Iniciar partida
+2- Historial
+3- Salir
+"""
+
+# Solicita y carga los nombres de los jugadores en la lista de jugadores.
+def cargar_nombres() -> None:
+    for i in range(2):
+        jugador = input(f"Ingrese el nombre del Jugador {i + 1}: ")
+        lista_jugadores[i].nombre = jugador
+
+# Permite al jugador seleccionar un luchador de la lista de luchadores.
+def elegir_luchador() -> None:
+    luchadores_disponibles = luchadores.copy()
+    
+    for jugador in range(2):
+        opcion_valida = False
+        while not opcion_valida:
+            try:
+                print(f"\nLuchadores disponibles:")
+                for i, luchador in enumerate(luchadores_disponibles):
+                    print(f"{i + 1}- {luchador}")
+                
+                indice = int(input(f"{lista_jugadores[jugador].nombre}, seleccione un luchador: "))
+                
+                if 1 <= indice <= len(luchadores_disponibles):
+                    luchador_elegido = luchadores_disponibles[indice - 1]
+                    lista_jugadores[jugador].personaje = luchador_elegido
+                    print(f"{lista_jugadores[jugador].nombre} eligió a {luchador_elegido.nombre}")
+                    
+                    # Remover el luchador elegido de los disponibles
+                    luchadores_disponibles.pop(indice - 1)
+                    
+                    opcion_valida = True
+                else:
+                    print("Luchador inexistente. Intente de nuevo.")
+            except ValueError:
+                print("Ingrese un número válido. Intente de nuevo.")
+
+        
+        
+
+# Inicia una nueva partida entre los dos jugadores.
+def iniciar_partida() -> None:
+    elegir_luchador()
+    partida = jugar
+    partida.comenzar_partida()
+
+def programa_principal() -> None:
+    cargar_nombres()
+    opcion_valida = False
+
+    while not opcion_valida:
+        try:
+            print(menu())
+            opcion = int(input("Ingrese una opción: "))
+            if 1 <= opcion <= 3:
+                if opcion == 1:
+                    iniciar_partida()
+                elif opcion == 2:
+                    print("\nHistorial")
+                    for i, jugador in enumerate(lista_jugadores, 1):
+                        print(f"{i} - {lista_jugadores[i-1].nombre} - {jugador}")
+                
+                elif opcion == 3:
+                    print("¡Adios!")
+                    opcion_valida = True
+            else:
+                print("Opción inexistente. Intente de nuevo.")
+        except ValueError:
+            print("Ingrese un número válido. Intente de nuevo.")
+
+programa_principal()
---
+++ TPI-Mortal-Kombat/partida.py
@@ -0,0 +1,96 @@
+from jugador import Jugador
+from escenario import Escenario
+import random
+import time
+
+class Partida:
+    
+    __id_partida = 0
+    
+    def __init__(self, jugador1: Jugador, jugador2: Jugador, escenario: Escenario) -> None:
+        self.__jugador1 = jugador1
+        self.__jugador2 = jugador2
+        self.__escenario = escenario
+        self.__id_partida = Partida.aumentar_id()
+
+    @property
+    def jugador1(self) -> Jugador:
+        return self.__jugador1
+
+    @jugador1.setter
+    def jugador1(self, nuevo_jugador1: Jugador) -> None:
+        self.__jugador1 = nuevo_jugador1
+
+    @property
+    def jugador2(self) -> Jugador:
+        return self.__jugador2
+
+    @jugador2.setter
+    def jugador2(self, nuevo_jugador2: Jugador) -> None:
+        self.__jugador2 = nuevo_jugador2
+
+    @property
+    def escenario(self) -> Escenario:
+        return self.__escenario
+
+    @escenario.setter
+    def escenario(self, nuevo_escenario: Escenario) -> None:
+        self.__escenario = nuevo_escenario
+
+    @property
+    def id_partida(self) -> int:
+        return self.__id_partida
+
+    @classmethod
+    def aumentar_id(cls) -> int:
+        cls.__id_partida += 1
+        return cls.__id_partida
+
+    def comenzar_partida(self) -> None:
+        luchador1 = self.jugador1.personaje
+        luchador2 = self.jugador2.personaje
+        lista_luchadores = [luchador1, luchador2]
+        juego_posible = True
+
+        print(self)
+        for i in range(3, 0, -1):
+            print(f"     {i}")
+            time.sleep(1.5)
+        print("¡¡ FIGHT !!\n")
+
+        while juego_posible:
+            atacante = random.choice(lista_luchadores)
+            enemigo = luchador2 if atacante == luchador1 else luchador1
+             
+            if luchador1.salud < 30 or luchador2.salud < 30:
+                if self.escenario.trampa_mortal:
+                    print(f"{atacante.nombre} lazó a {enemigo.nombre} a la Trampa Mortal")
+                    print("TRAMPA MORTAL ACTIVA")
+                    self.escenario.activar_trampa(enemigo)
+
+            lista_ataques = [atacante.ataque_basico, atacante.ataque_arma, atacante.ataque_especial]
+            ataque_elegido = random.choice(lista_ataques)
+            time.sleep(1.5)
+            ataque_elegido(enemigo)
+
+            print(f"""
+Salud {luchador1.nombre}: {luchador1.salud}%
+Salud {luchador2.nombre}: {luchador2.salud}%
+""")
+
+            if luchador1.muerto() or luchador2.muerto():
+                juego_posible = False
+                if luchador1.salud != 0:
+                    print(f"{luchador1.nombre} is WIN")
+                    self.jugador1.partidas_ganadas += 1
+                else:
+                    print(f"{luchador2.nombre} is WIN")
+                    self.jugador2.partidas_ganadas += 1
+
+        luchador1.salud = 100
+        luchador1.poder = 0
+        luchador2.salud = 100
+        luchador2.poder = 0
+
+    def __str__(self) -> str:
+        return f"\nPartida: {self.id_partida} - {self.jugador1.personaje.nombre} VS {self.jugador2.personaje.nombre} en {self.escenario}"
